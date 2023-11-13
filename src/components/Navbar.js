2865b6e51e12716ba47419f20c0295619570c796
deepakjangid12 <143808793+deepakjangid12@users.noreply.github.com>
2023-11-13 12:55:59 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,114 @@
-import React from "react"
-import { Link } from "react-router-dom"
+
+
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar(props) {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
+  const closeNav = () => {
+    setIsNavCollapsed(true);
+  };
+
   return (
-        <div> 
-          <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark ">
-            <div className="container-fluid">
-             <Link className="navbar-brand" to="/deep">{props.title}</Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                <li className="nav-item">
-                   <Link className="nav-link" to="/">Home</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to="/business">Business</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to='/health'>Health</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to='/science'>Science</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to='/sports'>Sports</Link>
-                  </li>
-                  <li className="nav-item">
-                   <Link className="nav-link" to='/technology'>Technology</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-
-
+    <div>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/" onClick={closeNav}>
+            {props.title}
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed ? true : false}
+            aria-label="Toggle navigation"
+            onClick={handleNavCollapse}
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className={`${
+              isNavCollapsed ? "collapse" : ""
+            } navbar-collapse`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/"
+                  onClick={closeNav}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/business"
+                  onClick={closeNav}
+                >
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/entertainment"
+                  onClick={closeNav}
+                >
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/health"
+                  onClick={closeNav}
+                >
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/science"
+                  onClick={closeNav}
+                >
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/sports"
+                  onClick={closeNav}
+                >
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="/technology"
+                  onClick={closeNav}
+                >
+                  Technology
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-  )
+      </nav>
+    </div>
+  );
 }
-
-
 
